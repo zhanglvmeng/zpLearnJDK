e55bda86ccddeb6644d5dea01ff4557f6a4dfc11
zhangpeng <zhangpeng@ushareit.com>
2020-06-19 09:02:37 +0800

    实验了一下instanceOf

--- src/test/Number/TestDouble.java
+++ src/test/Number/TestDouble.java
@@ -3,13 +3,21 @@ package test.Number;
 public class TestDouble {
 
     public static void main(String[] args) {
-        Double d1 = 0.01;
-        Double d2 = 0.06;
-        Double d3 = 0.07;
-        System.out.println(d1 + d2);
-        System.out.println(d3 == d1 + d2);
+//        Double d1 = 0.01;
+//        Double d2 = 0.06;
+//        Double d3 = 0.07;
+//        System.out.println(d1 + d2);
+//        System.out.println(d3 == d1 + d2);
+//
+//        d1.compareTo(d2);
 
-        d1.compareTo(d2);
+        Object o = (Object) new Double(1);
+        if (o instanceof Double) {
+            System.out.println("true");
+        }
+
+
+
 
     }
 }

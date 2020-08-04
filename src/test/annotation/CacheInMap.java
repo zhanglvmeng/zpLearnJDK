e010776bc230cc4da33df589f150dcfde82c0939
zhangpeng <zhangpeng@ushareit.com>
2020-08-04 09:51:40 +0800

    添加 注解默认key

--- src/test/annotation/CacheInMap.java
+++ src/test/annotation/CacheInMap.java
@@ -6,5 +6,5 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.METHOD, ElementType.ANNOTATION_TYPE })
 public @interface CacheInMap {
-    String key() default "";
+    String key() default "default key";
 }
